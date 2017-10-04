c20c49bcfaa5054d184abbc41c42312449fab4b7
Jake Dluhy <dluhy.jake@gmail.com>
2017-10-03 17:31:35 -0700

    Add a simple magnitude utility for 2D points passed in as arrays

--- utils/math.js
+++ utils/math.js
@@ -6,5 +6,13 @@ function sumN(n) {
   return (n * (n + 1)) / 2;
 }
 
+function mag(p1, p2) {
+  return Math.sqrt(
+    Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2)
+  );
+}
+
 exports.randomBetween = randomBetween;
 exports.sumN = sumN;
+exports.mag = mag;
+exports.sqrt3 = Math.sqrt(3);
