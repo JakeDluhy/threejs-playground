818f967baed86ba15c5008d4b1ecb4a44a5e0e99
Jake Dluhy <dluhy.jake@gmail.com>
2017-10-04 14:20:11 -0700

    Refactor ocean to have separate params file and directory

--- tiles/ocean/index.js
+++ tiles/ocean/index.js
@@ -1,12 +1,14 @@
 const THREE = require('three');
 
-const { createGenericTile } = require('./generic');
-const createWavyGround = require('./partials/create-wavy-ground');
+const { createGenericTile } = require('../generic');
+const createWavyGround = require('../partials/create-wavy-ground');
 
-const OCEAN_COLOR = new THREE.Color(40/255, 90/255, 170/255);
-const WAVE_HEIGHT = 1;
-const WAVE_OFFSET = 4
-const NUM_WAVES =   6;
+const {
+  OCEAN_COLOR,
+  WAVE_HEIGHT,
+  WAVE_OFFSET,
+  NUM_WAVES,
+} = require('./params');
 
 function createPlainsTile() {
   const group = createGenericTile();
---
+++ tiles/ocean/params.js
@@ -0,0 +1,8 @@
+const THREE = require('three');
+
+module.exports = {
+  OCEAN_COLOR: new THREE.Color(40/255, 90/255, 170/255),
+  WAVE_HEIGHT: 1,
+  WAVE_OFFSET: 4,
+  NUM_WAVES:   6,
+};
